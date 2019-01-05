848aac46f92657085c36bf8ff100897c3e6d4675
Valentin Salome <vasalome@student.le-101.fr>
2019-01-05 17:24:13 +0100

    add_condition_mandelbrot

--- srcs_fractol/main_fractol.c
+++ srcs_fractol/main_fractol.c
@@ -6,10 +6,14 @@
 /*   By: vasalome <vasalome@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/04 16:25:04 by vasalome     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/04 17:04:59 by vasalome    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/05 17:23:15 by vasalome    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
+
+#include "./minilibx_macos/mlx.h"
+#include "./libft/includes/libft.h"
+#include <stdio.h>
 
 /*
 typedef struct  s_fractol
@@ -24,12 +28,29 @@ typedef struct  s_fractol
 
 int     main(int argc, char **argv)
 {
-    float       c_r = x;
-    float       c_i = y;
-    float       z_r = 0;
-    float       z_i = 0;
+    int         x1 = -2.1;
+    int         x2 = 0.6;
+    int         y1 = -1.2;
+    int         y2 = 1.2;
+    int         zoom = 100;
     int         iteration_max = 50;
-    int        i = 0;
+
+    int         image_x = (x2 - x1) * zoom;
+    int         image_y = (y2 - y1) * zoom;
+
+    if (x = 0 && x < image_x)
+    {
+        if (y = 0 && y < image_y)
+        {
+            float       c_r = x / zoom + x1;
+            float       c_i = y / zoom + y1;
+            float       z_r = 0;
+            float       z_i = 0;
+            int        i = 0;
+        }
+    }
+
+
     float   tmp = z_r;
     while (((z_r * z_r + z_i * z_i)) < 4 && (i < iteration_max))
     {
@@ -39,4 +60,6 @@ int     main(int argc, char **argv)
     }
     if (i == iteration_max)
         mlx_putpixel aux coordonnees x;y
+
+    mlx_loop(data->mlx);
 }
